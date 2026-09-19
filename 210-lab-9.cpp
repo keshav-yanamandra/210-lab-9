ac4cfd5e6728b2204ea0393ffb93455753c4747c
Keshav Yanamandra <keshav.yanamandra@gmail.com>
2026-09-19 11:53:36 -0700

    opened the file to read

--- 210-lab-9.cpp
+++ 210-lab-9.cpp
@@ -7,7 +7,36 @@
 #include <array>
 using namespace std;
 
+const int SIZE = 38;
+
+
 int main() {
+
+    cout << "Barcelona goals: ";
+    cout << "1, 2, 3";
+    cout << endl;
+
+    array<int, SIZE> goals;
+    ifstream fin;
+    int line=0;
+    fin.open("goals.txt");
+
+        if (fin.good()) {
+            cout << line;
+            line++;
+
+
+        fin.close();
+    }
+    else {
+        cout << "file not found." << endl;
+    }
+
+
+
+    cout << endl;
+
+
 
     return 0;
 }
